38f4ce556fdf731375b7b29163dcc0fcd2766166
Denise Case <dmcase@ksu.edu>
2012-12-08 09:04:11 -0600

    generic sys calls

--- test/test.c
+++ test/test.c
@@ -1,12 +1,38 @@
-#include <stdio.h>
-#include <linux/unistd.h>
-#include <sys/syscall.h>
-#include <linux/prinfo.h>
+//#include <stdio.h>
+//#include <linux/unistd.h>
+//#include <sys/syscall.h>
+//#include <linux/prinfo.h>
 
-int main(void)
+#include <stdio.h>
+#include <unistd.h>
+#include <asm/unistd.h>
+#include <syscall.h>
+#include <sys/types.h>
+#include "prinfo.h"
+
+int main()
 {
-   int entries;
-   printfo * buf;
-   int * nr;
-   entries = syscall(sys_ptree, buf, nr);
+   // int entries;
+   // printfo * buf;
+   // int * nr;
+   // entries = syscall(sys_ptree, buf, nr);
+
+   struct prinfo procInfo[3];
+   int n = 3;
+   int p;
+
+   p = syscall(__NR_getpid);
+   printf("pid = %d\n", p);
+   printf("__NR_getpid = %d\n", __NR_getpid);
+
+   p = syscall(223,procInfo,&n);
+
+  // ptree(procInfo,&n);
+
+   printf("procInfo[0].pid = %d\n", procInfo[0].pid);
+   printf("procInfo[0].uid = %ld\n", procInfo[0].uid);
+  
+   printf("p= %d\n", p);
+
+   return 0;
 }
---
+++ test/testcall.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <asm/unistd.h>
+#include <syscall.h>
+#include <sys/types.h>
+#include "prinfo.h"
+
+
+/** Display process information for up to three entries
+in depth-first search order.  Display information about
+process name, pid, parent pid, two siblings, and process
+owner.  If a role is empty (e.g. there is no child), use 0.
+*/
+
+int main()
+{
+        struct prinfo procInfo[3];   
+        int n = 3;
+        int p;
+
+        p = syscall(__NR_getpid);
+        printf("pid = %d\n", p);
+        printf("__NR_getpid = %d\n", __NR_getpid);
+
+        p = syscall(223,procInfo,&n);
+
+//      ptree(procInfo,&n);
+
+        printf("procInfo[0].pid = %d\n", procInfo[0].pid);
+        printf("procInfo[0].uid = %ld\n", procInfo[0].uid);
+
+        printf("p= %d\n", p);
+
+        return 0;
+};
